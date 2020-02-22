af8d3a15b0ba7cfdf52761cba98271451838b104
57230960 <jordan.fogg@okstate.edu>
2020-02-22 10:28:38 -0600

    .

--- test.py
+++ test.py
@@ -1,41 +1,60 @@
 import cv2
-import imutils
 import numpy as np
-import matplotlib
-img = cv2.imread('IMG_7298sm.jpg',1)
-dim = (172, 300)
-img2 = cv2.resize(img, dim)
-img5 = img2
-while (1):
-    hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
-    lower_value1 = np.array([5,121,210])
-    upper_value1 = np.array([25,171,255])
+import imutils
 
-    lower_value3 = np.array([0,164,133])   
-    upper_value3 = np.array([30,255,255])
+cap = cv2.VideoCapture(0)
+
+while(1):
+
+    # Take each frame
+    _, frame = cap.read()
+
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+ 
+    lower_value1 = np.array([5,121,180])
+    upper_value1 = np.array([50,210,255])
+
+    lower_value3 = np.array([20,120,50])
+    upper_value3 = np.array([200,255,170])
 
 
     mask_color1 = cv2.inRange(hsv, lower_value1, upper_value1)
     mask_color3 = cv2.inRange(hsv, lower_value3, upper_value3)
     mask = mask_color1 + mask_color3
-    res = cv2.bitwise_and(img2,img2, mask= mask)
-    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(contours)
-   # M = cv2.moments(cnt)
-   # cx = int(M['m10']/M['m00'])
-    #cy = int(M['m01']/M['m00'])
-    #show = cv2.convexHull(cnts[-1])
+   
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    edge = cv2.Canny(res,100,200)
+    kernel = np.ones((10,10),None)
+    kernel2 = np.ones((25,25),None)
+    opening = cv2.morphologyEx(mask,cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening,cv2.MORPH_CLOSE, kernel2)
 
-   # cnts = []
-   #while(c < 5)
-      #  x,y,w,h = cv2.boundingRect(cnts[0])
-      #  cv2.rectangle(img5,(x,y),(x+w,y+h),(0,255,0),2)
-        
-    #   cv2.drawContours(mask,cnts,-1,(0,255,0),3)
-    cv2.imshow('IMG_7298sm.jpg', img2)
-    cv2.imshow("mask", mask) 
-    cv2.imshow("bitwise", res)
-    key = cv2.waitKey(1) & 0xFF
+    cnt = cv2.findContours(closing,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnt)
+    center = None
+    if len(cnts)>0:
 
-    if key == ord('q'):
+        c = max(cnts, key=cv2.contourArea)
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 2:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0,0,255), -1)
+    cv2.imshow('clean fix',closing)
+    #cv2.imshow('clean', opening)
+   # cv2.imshow('edge',edge)
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    print(center)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
         break
+
+cv2.destroyAllWindows()
+
